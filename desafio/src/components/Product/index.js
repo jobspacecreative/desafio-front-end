d622721041baa87a08dced89926d8676816aff9e
Filipi Rafael <filipirafael.123@gmail.com>
2022-08-03 21:12:20 -0300

    Add listagem da api de forma dinâmica

--- desafio/src/components/Product/index.js
+++ desafio/src/components/Product/index.js
@@ -7,17 +7,17 @@ import { BoxCard, Price } from './styles';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-export default function Product() {
+export default function Product({ avatar, name, desciption, price }) {
 
     return(
         <BoxCard>
             <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="http://www.receitadodia.com/wp-content/uploads/2013/07/bolo_chocolate-521x380.jpg" />
+                <Card.Img variant="top" src={avatar} />
                 <Card.Body>
-                    <Card.Title>Bolo de chocolate</Card.Title>
+                    <Card.Title>{name}</Card.Title>
                     <Card.Text>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus condimentum ipsum vitae auctor. 
-                    <Price>129.60</Price>
+                    {desciption}
+                    <Price>{price}</Price>
                     </Card.Text>
                     <Button variant="primary">Adicionar ao carrinho</Button>
                 </Card.Body>
--- desafio/src/pages/Home/index.js
+++ desafio/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Row,
     Col
@@ -9,7 +9,22 @@ import {
   import Header from '../../components/Header'
   import Product from '../../components/Product'
 
+  import api from '../../services/api';
+
   export default function Home() {
+
+    const [productsList, setProductsList] = useState([]);
+
+    const getProducts = async () => {
+      await api.get()
+      .then((response) => setProductsList(response.data))
+      .catch((err) => console.error(err));
+    }
+
+    useEffect(() => {
+      getProducts();
+    }, []);
+
       return(
         <>
           <Container>
@@ -19,12 +34,15 @@ import {
               </Col>
               <h2>Produtos</h2>
               <BoxProducts>
-                <Product props/>
-                <Product props/>
-                <Product props/>
-                <Product props/>
-                <Product props/>
-                <Product props/>
+                {productsList.map((product) => (
+                  <Product
+                    key={product.id}
+                    avatar={product.avatar}
+                    name={product.name}
+                    desciption={product.desciption}
+                    price={product.price}
+                  />
+                ))}
               </BoxProducts>  
             </Row>
           </Container>
